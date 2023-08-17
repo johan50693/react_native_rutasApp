0f70b4e1a807bf77e0e99104a853a5c2a23c9f11
Albert Urbina <johan50693@gmail.com>
2023-08-16 21:45:19 -0400

    Estructura inicial

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const App = () => {
+  return (
+    <View>
+      <Text> App </Text>
+      <Icon name="search-outline" size={20} />
+    </View>
+  );
+};
+
+export default App;

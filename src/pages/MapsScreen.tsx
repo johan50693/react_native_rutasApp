31c970ecb4f47444164f59bf8b83fb93ce6187b8
Albert Urbina <johan50693@gmail.com>
2023-08-30 12:55:18 -0400

    Se realoiza la configuracion e instalacion del mapa para android

--- src/pages/MapsScreen.tsx
+++ src/pages/MapsScreen.tsx
@@ -1,11 +1,21 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export const MapsScreen = () => {
   return (
-      <View>
-        <Text> MaaaaaaapsScreen </Text>
+      <View style={{flex: 1}}>
+        <Text>Hola mundoooo</Text>
+        <MapView
+        style= {{flex: 1}}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
       </View>
   );
 };
